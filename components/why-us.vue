d99fe43ea59e7c7c060d0053e2cba6751d12ef57
DanyalBani <mr.d.bani.d@gmail.com>
2021-10-03 18:09:44 +0330

    why-us created

--- components/why-us.vue
+++ components/why-us.vue
@@ -1,7 +1,54 @@
 <template>
-  <div class="bg-primary">
-    <div class="flex">
-      fuck
+  <div class="bg-primary text-Neutral-white w-11/12 mx-auto pt-20">
+    <div class="flex justify-between">
+      <div class="flex items-center">
+        <div
+          class=" w-16 h-16 bg-Neutral-white rounded-full bg-opacity-10 justify-center items-center flex"
+        >
+          <img class="w-7" src="../assets\images\charticon.png" alt="chart" />
+        </div>
+        <div class="ml-5">
+          <p class="text-4xl font-extrabold">$30B</p>
+          <p class="mt-2">Digital Currencyy Exchanged</p>
+        </div>
+      </div>
+      <div class="flex items-center">
+        <div
+          class="w-16 h-16 bg-Neutral-white rounded-full bg-opacity-10 justify-center items-center flex"
+        >
+          <img class="w-7" src="../assets\images\usericon.png" alt="user" />
+        </div>
+        <div class="ml-5">
+          <p class="text-4xl font-extrabold">10M+</p>
+          <p class="mt-2">Trusted Wallets Investor</p>
+        </div>
+      </div>
+      <div class="flex items-center mr-2">
+        <div
+          class="w-16 h-16 bg-Neutral-white rounded-full bg-opacity-10 justify-center items-center flex"
+        >
+          <img class="w-7" src="../assets\images\globalicon.png" alt="global" />
+        </div>
+        <div class="ml-5">
+          <p class="text-4xl font-extrabold">195</p>
+          <p class="mt-2">Countries Supported</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex mt-10 justify-between items-center">
+      <div class="w-7/12">
+        <img src="../assets\images\why-us.png" alt="" />
+      </div>
+      <div class="flex flex-col ml-10">
+        <p class="text-5xl mb-5">Why you should choose CRAPPO</p>
+        <p class="my-5 text-Neutral-Grey5">
+          Experience the next generation cryptocurrency <br> platform. No financial
+          borders, extra fees, and fake <br> reviews.
+        </p>
+        <div class="mt-5">
+          <Button blue="true" name="Learn More" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
