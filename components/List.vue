bb24d1f4acdd0e98135829eaa2cc36c09948bec2
DanyalBani <mr.d.bani.d@gmail.com>
2021-10-17 11:05:17 +0330

    adding style to List and List Item

--- components/List.vue
+++ components/List.vue
@@ -1,10 +1,15 @@
 <template>
-  <div style="background-color:#35068C; " class="flex flex-col">
+  <div
+    style="background-color:#35068C; "
+    class="flex flex-col px-10 py-6 rounded-xl ml-52 w-full"
+  >
     <div class="flex txet-Neutral-white justify-between text-Neutral-white">
-      <p class="flex-1" v-for="header in headers" :key="header">{{ header }}</p>
+      <p class="flex-1 text-center" v-for="header in headers" :key="header">
+        {{ header }}
+      </p>
     </div>
     <div>
-      <div v-for="item in items" :key="item.id">
+      <div v-for="item in items" class="py-6 " :key="item.id">
         <single-list-item
           :LogoName="item.LogoName"
           :price="item.price"
--- components/Single-list-item.vue
+++ components/Single-list-item.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="flex ">
-    <div class="flex">
+  <div class="flex w-full itemho text-center justify-center items-center pl-2 py-2 rounded">
+    <div class="flex flex-1 text-left">
       <span v-if="LogoName">
         <img class="w-12" :src="baseSrc + LogoName + '.png'" :alt="LogoName" />
       </span>
-      <div>
+      <div class="ml-3">
         <p class="text-Neutral-white">{{ abbr }}</p>
         <p class="text-Neutral-Grey6">{{ abbreviation }}</p>
       </div>
     </div>
-    <div class="text-Neutral-Grey6">{{ price }}</div>
-    <div class="change">{{ change }}</div>
-    <div class="text-Neutral-Grey6">{{ volume }}</div>
+    <div class="text-Neutral-Grey6 flex-1 text-center">{{ price }}</div>
+    <div class="change flex-1">{{ change }}</div>
+    <div class="text-Neutral-Grey6 flex-1">{{ volume }}</div>
   </div>
 </template>
 
@@ -29,5 +29,12 @@ export default {
 <style>
 .change {
   color: #8fffbe;
+}
+.itemho {
+  transition: all 0.5s ease-in-out;
+}
+.itemho:hover {
+  transform: translateX(-2rem);
+  background-color: #4610ad;
 }
 </style>
