dc70dca91dc8374daa49770f63861d55acc354a5
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-16 17:41:37 +0330

    so know we don't get the overflow because fade of aos

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="pt-10 pb-20 bg-primary">
+  <div class="pt-10 pb-20 bg-primary overflow-x-hidden hidden-s">
     <nuxt />
   </div>
 </template>
+<style scoped>
+
+</style>
